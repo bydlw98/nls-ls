08f04963277ed2c6afb918d54af70c816528576a
bydlw98 <108581910+bydlw98@users.noreply.github.com>
2024-04-07 22:42:40 +0800

    chore: release v0.4.1

--- build.rs
+++ build.rs
@@ -47,7 +47,7 @@ fn generate_manpage() {
     }
     let cmd = build_command();
 
-    let man = clap_mangen::Man::new(cmd).date("2023-12-02");
+    let man = clap_mangen::Man::new(cmd).date("2024-07-04");
     let mut buffer: Vec<u8> = Default::default();
     man.render(&mut buffer)
         .expect("Unable to render man page to buffer");
